5adabd1fc8eb3ccdbe0753c254f62f6d66bd1c2d
Devesh Sharma <deveshsharma372002@gmail.com>
2025-05-10 10:12:32 +0530

    getAllUsers api for admin only

--- NodeBackend/src/app.js
+++ NodeBackend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 const {connectDB} =require('./config/db')
 const authRouter = require('./routes/auth');
+const userRouter = require('./routes/users');
 const app = express();
 
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 
-app.use("/",authRouter)
+app.use("/",authRouter,userRouter)
 
 
 // Directly call connectDB without promises
