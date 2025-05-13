b1bf8810c9b62ea5a655bd25b67241dc360307c7
Devesh Sharma <deveshsharma372002@gmail.com>
2025-05-13 16:21:36 +0530

    update toast message

--- FrontEnd/ComplaintPortal/src/App.jsx
+++ FrontEnd/ComplaintPortal/src/App.jsx
@@ -1,21 +1,22 @@
 import { Outlet } from "react-router"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-
+import { useSelector } from "react-redux"
 
 
 
 function App() {
+const isLoggedIn  = useSelector(store=> store.user.isLoggedIn)
 
 
   return (
     <>
       <div className="min-h-screen flex flex-col">
-        <Header />
+       {isLoggedIn && <Header />}
         <main className="flex-1">
           <Outlet />
         </main>
-        <Footer />
+       {isLoggedIn && <Footer />}
       </div>
 
 
--- FrontEnd/ComplaintPortal/src/hooks/useSignInWithEmailAndPassword.js
+++ FrontEnd/ComplaintPortal/src/hooks/useSignInWithEmailAndPassword.js
@@ -1,53 +1,67 @@
-import {toast} from "react-toastify";
+import { Bounce, toast } from "react-toastify";
 import { useCallback } from "react";
 import loginValidation from "../utils/validation/loginValidation";
 import { useDispatch } from "react-redux";
 import { addUser } from "../redux/slice/userSlice";
 import { BASE_URL } from "../utils/constants";
 
-const useSignInWithEmailAndPassword =()=>{
+const useSignInWithEmailAndPassword = () => {
     const dispatch = useDispatch();
 
-  const loginHandler = useCallback(async (emailRef, passwordRef, navigate) => {
-    try {
-      const Email = emailRef.current?.value;
-      const Password = passwordRef.current?.value;
-      if(!Email || !Password) {
-        toast.error("Both fields are required", { theme: "dark"});
-        return;
-      }
-      const isValid = loginValidation(Email, Password);
-      if (!isValid) {
-        toast.error("Credentials are invalid", { theme: "dark" });
-        return;
-      }
+    //   const loginHandler = 
+    return useCallback(async (emailRef, passwordRef, navigate) => {
+        try {
+            const Email = emailRef.current?.value;
+            const Password = passwordRef.current?.value;
+            if (!Email || !Password) {
+                toast.error("Both fields are required", { theme: "dark" });
+                return;
+            }
+            const isValid = loginValidation(Email, Password);
+            if (!isValid) {
+                toast.error("Credentials are invalid", { theme: "dark" });
+                return;
+            }
 
-      let loggedInUser = await fetch(BASE_URL + "/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ Email, Password }),
-        credentials: "include"
-      });
+            let loggedInUser = await fetch(BASE_URL + "/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ Email, Password }),
+                credentials: "include"
+            });
 
-      loggedInUser = await loggedInUser.json();
+            loggedInUser = await loggedInUser.json();
 
-      if (loggedInUser.error === undefined) {
-        //just for testing purpose
-        localStorage.setItem("email", Email);
-        localStorage.setItem("password", Password);
-        //for redux store
-        dispatch(addUser(loggedInUser.user));
-        navigate("/");
-      } else {
-        toast.error(loggedInUser.error, { theme: "dark" });
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error(error.message, { theme: "dark" });
-    }
-  }, [dispatch]);
+            if (loggedInUser.error === undefined) {
+                //just for testing purpose
+                localStorage.setItem("email", Email);
+                localStorage.setItem("password", Password);
+                //for redux store
+                dispatch(addUser(loggedInUser.user));
+                toast('🦄 Wow Logged in', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                });
+                setTimeout(() => {
+                    navigate("/");
+                }, 2000)
+            } else {
+                toast.error(loggedInUser.error, { theme: "dark" });
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message, { theme: "dark" });
+        }
+    }, [dispatch]);
 
-  return loginHandler;
+    //   return loginHandler;
 }
 
 
