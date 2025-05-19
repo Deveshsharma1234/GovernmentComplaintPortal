de1dfa0fea81a8c052628c69a565191d88e1043a
Devesh Sharma <deveshsharma372002@gmail.com>
2025-05-19 08:47:09 +0530

    resolved dependecies

--- Backend_Net/ComplaintPortal/ComplaintPortal/Controllers/AuthController.cs
+++ Backend_Net/ComplaintPortal/ComplaintPortal/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ComplaintPortal.Business.Contracts;
+using ComplaintPortal.Entities.DTO;
+using Microsoft.AspNetCore.Authorization;
 namespace ComplaintPortal.Controllers
 {
     [Route("api/[controller]")]
@@ -14,5 +16,41 @@ namespace ComplaintPortal.Controllers
         {
             _service = service;
         }
+
+
+
+
+        [HttpPost("citizen-register")]
+        public async Task<IActionResult> RegisterCitizen(RegisterCitizenDto dto)
+        {
+            try
+            {
+                var user = await _service.RegisterCitizenAsync(dto);
+                return Ok(new { message = "User Created Successfully", user });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("admin/register")]
+        //[Authorize(Roles = "Admin")] // Requires JWT middleware config
+        public async Task<IActionResult> RegisterAdmin(RegisterAdminDto dto)
+        {
+            try
+            {
+                var user = await _service.RegisterAdminAsync(dto);
+                return Ok(new { message = "Admin Created Successfully", user });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+
+
+
     }
 }
--- Backend_Net/ComplaintPortal/ComplaintPortal/Program.cs
+++ Backend_Net/ComplaintPortal/ComplaintPortal/Program.cs
@@ -1,4 +1,11 @@
 
+using ComplaintPortal.Business.Classes;
+using ComplaintPortal.Business.Contracts;
+using ComplaintPortal.DataAccess.Repository.Classes;
+using ComplaintPortal.DataAccess.Repository.Contracts;
+using ComplaintPortal.Entities.EFCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace ComplaintPortal
 {
     public class Program
@@ -13,6 +20,25 @@ namespace ComplaintPortal
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            #region Register Dependencies
+
+            //Configure Entity Framework and MySQL database context
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+)
+
+           );
+
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+
+
+
+            #endregion
 
             var app = builder.Build();
 
