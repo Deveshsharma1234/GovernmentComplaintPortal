1b85cecf34f5834c6cb758d34f543d351430d446
Devesh Sharma <deveshsharma372002@gmail.com>
2025-05-20 15:12:08 +0530

    City Controller

--- Backend_Net/ComplaintPortal/ComplaintPortal.Entities/DTO/CityResoponseDto.cs
+++ Backend_Net/ComplaintPortal/ComplaintPortal.Entities/DTO/CityResoponseDto.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ComplaintPortal.Entities.DTO
 {
-    class CityResoponseDto
+    public class CityResoponseDto
     {
+        public int CityID { get; set; }
+        public string City { get; set; }
+        public int DistrictID { get; set; }
+        public int StateID { get; set; }
+        public string CreatedBy  { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int  ActiveStatus { get; set; }
+
     }
 }
+
+
+/*  
+ *          "CityID": 1,
+            "City": "SGM",
+            "DistrictID": 508,
+            "StateID": 21,
+            "CreatedBy": "system",
+            "CreatedDate": "2025-05-14T19:23:00.000Z",
+            "ModifiedBy": "system",
+            "ModifiedDate": "2025-05-14T19:23:00.000Z",
+            "ActiveStatus": 1
+*/
+
